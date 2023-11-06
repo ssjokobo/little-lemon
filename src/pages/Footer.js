0b5eed64b100b2cdc169ed12f31aa708e87c1773
Sopon Suwannakit <soponsuwannakit@SoupBook-Air.local>
2023-11-06 13:04:35 -0600

    add css for footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,28 +1,36 @@
 function Footer() {
     return (
         <footer className="grid-container">
-            <a><img src="assets/images/logo.png" width="200"/></a>
-            <h3>Doormat Navigation</h3>
-                <ul>
-                    <a>Home</a>
-                    <a>About</a>
-                    <a>Menu</a>
-                    <a>Reservations</a>
-                    <a>Order Online</a>
-                    <a>Login</a>
-                </ul>
-            <h3>Contact</h3>
-                <ul>
-                    <p>Address</p>
-                    <p>Phone Number</p>
-                    <p>Email</p>
-                </ul>
-            <h3>Social Media Links</h3>
-                <ul>
-                    <p>Address</p>
-                    <p>Phone Number</p>
-                    <p>Email</p>
-                </ul>
+            <div className="grid-item">
+                <a><img src="assets/images/logo.png" width="200"/></a>
+            </div>
+            <div className="grid-item">
+                <h3>Doormat Navigation</h3>
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Menu</a></li>
+                        <li><a>Reservations</a></li>
+                        <li><a>Order Online</a></li>
+                        <li><a>Login</a></li>
+                    </ul>
+            </div>
+            <div className="grid-item">
+                <h3>Contact</h3>
+                    <ul>
+                        <li><a>Address</a></li>
+                        <li><a>Phone Number</a></li>
+                        <li><a>Email</a></li>
+                    </ul>
+            </div>
+            <div className="grid-item">
+                <h3>Social Media Links</h3>
+                    <ul>
+                        <li><a>Address</a></li>
+                        <li><a>Phone Number</a></li>
+                        <li><a>Email</a></li>
+                    </ul>
+            </div>
         </footer>
     );
   }
